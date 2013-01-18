2dedb449b895ce9319a7990fcda056a28bf064f5
houjing <miroshkin_dmitrij@hotmail.com>
2013-01-18 21:15:41 +0200

    Twig loaded.

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 
-require_once('utils.php'); $s = $_COOKIE['sessid'];
-if ( $s && strlen($s)==64 && sessionActive($s) ) refreshSession($s);
-insertEncoding('utf-8');
-include('inc/index');
+//require_once('utils.php'); $s = $_COOKIE['sessid'];
+//if ( $s && strlen($s)==64 && sessionActive($s) ) refreshSession($s);
+//insertEncoding('utf-8');
+//include('inc/index');
+
+require_once './Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_String();
+$twig = new Twig_Environment($loader);
+echo $twig->render('Hello {{ name }}!', array('name' => 'works'));
+
 
 ?>
