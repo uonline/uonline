21cc47cc84e585e7938c322c64d027ed812ab70a
houjing <miroshkin_dmitrij@hotmail.com>
2013-01-18 22:08:32 +0200

    First twig using

--- index.php
+++ index.php
@@ -6,21 +6,23 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('./templates');
 $twig = new Twig_Environment($loader, array(
 	// UNCOMMENT LATER
-    // 'cache' => './templates_cache',
-    'cache' => false,
+   // 'cache' => './templates_cache',
+   'cache' => false,
 ));
-
 ///// Do some actions.
 
-//require_once('utils.php'); $s = $_COOKIE['sessid'];
-//if ( $s && strlen($s)==64 && sessionActive($s) ) refreshSession($s);
+require_once('utils.php'); $s = $_COOKIE['sessid'];
+if ( $s && strlen($s)==64 && sessionActive($s) ) refreshSession($s);
 //insertEncoding('utf-8');
 //include('inc/index');
 
 
 ///// And render.
 echo $twig->render('index.twig', array(
-	'title' => 'Well'
+	'title' => 'Well',
+	'admin' => false,
+	'loggedIn' => sessionActive($s),
+	'login' => userBySession($s)
 ));
 
 ?>
