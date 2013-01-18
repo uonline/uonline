67bbdc5ccf71b91f89242fb43f74c3126b0fc309
m1kc <m1kc@yandex.ru>
2013-01-18 23:39:29 +0400

    Template test.

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
 require_once './Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_String();
-$twig = new Twig_Environment($loader);
-echo $twig->render('Hello {{ name }}!', array('name' => 'works'));
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader, array(
+	// UNCOMMENT LATER
+    // 'cache' => './templates_cache',
+));
 
+echo $twig->render('index.twig', array('title' => 'Well'));
 
 ?>
