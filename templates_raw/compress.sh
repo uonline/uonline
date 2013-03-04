df04a976e186bdd686fa447b8fd10c15a81650ad
m1kc <m1kc@yandex.ru>
2013-03-04 14:04:29 +0400

    Minified inline CSS.

--- templates_raw/compress.sh
+++ templates_raw/compress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in `ls | grep -v compress.sh`; do
 	echo "==>" htmlcompressor $i -o ../templates/$i --compress-css
-	htmlcompressor $i -o ../templates/$i
+	htmlcompressor $i -o ../templates/$i --compress-css
 done
