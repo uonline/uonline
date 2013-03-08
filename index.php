83cfc63ad68cddbcf43946270c814d4d066d6240
houjing <antidurov@mail.ru>
2013-03-08 19:44:17 +0200

    tf filter fixed references #51

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once './Twig/Autoloader.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('./templates');
 $twig = new Twig_Environment($loader, array('cache' => TWIG_CACHE));
-$twig->addFilter(new Twig_SimpleFilter('tf', 'tf'));
+$twig->addFilter(new Twig_SimpleFilter('tf', 'tf', array('pre_escape' => 'html', 'is_safe' => array('html') ) ) );
 $twig->addFilter( new Twig_SimpleFilter('nl2p', 'nl2p', array('pre_escape' => 'html', 'is_safe' => array('html') ) ) );
 
 
