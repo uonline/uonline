ac5674c93adc3f8b6f5e4359b898ba64b44d7862
houjing <antidurov@mail.ru>
2013-05-13 22:31:50 +0300

    closes #99 closes #103

--- index.php
+++ index.php
@@ -190,6 +190,17 @@ $app->get('/stats/', function () use ($app, $twig, $options, $s) {
 	return $twig->render( 'stats.twig', $options);
 });
 
+/********************** world **********************/
+$app->get('/world/', function () use ($app, $twig, $options, $s) {
+	goAttack($s);
+	return $twig->render( 'world.twig', $options);
+});
+
+/********************** guidelines **********************/
+$app->get('/develop/', function () use ($app, $twig, $options, $s) {
+	goAttack($s);
+	return $twig->render( 'develop.twig', $options);
+});
 
 /********************** others **********************/
 $app->error(function (Exception $e, $с) use ($app, $twig, $options, $s) {
