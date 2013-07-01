7859dab793d53f113419b8e1b801bcba11dd4475
m1kc (Max Musatov) <m1kc@yandex.ru>
2013-07-01 05:37:53 +0400

    Made the tests pass.

--- locparse.php
+++ locparse.php
@@ -1,92 +1,84 @@
 <?php
 
-	//$areas, $locations;
+class Area {
+	public $label, $name, $id;
+}
 
-	class Area {
-		public $label, $name, $id;
-	}
-	class Location {
-		public $label, $name, $description, $actions, $id;
-	}
+class Location {
+	public $label, $name, $description = "", $actions, $id;
+}
 
-	class Parser {
+class Parser {
 
-		public $areas, $locations;
+	public $areas = array(), $locations = array();
 
-		function processDir($dir, $previousLabel, $root) {
-			if (!$root) {
-				$a = new Area();
-				$a->label = split (split("/", $dir)[0], " - ")[1];
-				if ($previousLabel != null) $a->label = $previousLabel."-".$a->label;
-				$a->name = $dir.split("/")[1].split(" - ")[0];
-				$areas[count($areas)] = $a;
+	function processDir($dir, $previousLabel, $root) {
+		if ($root === false) {
+			$a = new Area();
+			$splittedDir = split("/", $dir);
+			$splittedStr = split(" - ", $splittedDir[count($splittedDir)-1]);
+			$a->label = $splittedStr[1];
+			if ($previousLabel != null) $a->label = $previousLabel."-".$a->label;
+			$a->name = $splittedStr[0];
+			$this->areas[] = $a;
 
-				processMap($dir."/map.ht.md", $a->label, $a->name);
-			}
-			foreach (dirEntries($dir, SpanMode.shallow) as $name)	{
-				if (isDir($name)) {
-					if ($root) {
-						processDir($name, null, false);
-					}
-					else {
-						processDir($name, end($areas)->label, false);
-					}
-				}
-			}
+			$this->processMap($dir."/map.ht.md", $a->label, $a->name);
 		}
-
-		function processMap($filename, $areaLabel, $areaName) {
-			foreach(readText(filename).split("\n") as $s) {
-				if (startsWith($s, "# ")) {
-					assert(substr($s, 2) == $areaName);
-				}
-				else if (startsWith(s, "### ")) {
-					$locations[] = new Location();
-					$tmp = substr($s, 4);
-					end($locations)->name = $tmp.split(" - ")[0];
-					end($locations)->label = $areaLabel . "/" . $tmp.split(" - ")[1];
-				}
-				else if (startsWith($s, "* ")) {
-					$tmp = substr($s, 2);
-					$tmpAction = $tmp.split(" - ")[0];
-					$tmpTarget = $tmp.split(" - ")[1];
-					if ($tmpTarget.indexOf('/') == -1) $tmpTarget = $areaLabel . "/" . $tmpTarget;
-					end($locations)->actions[$tmpAction] = $tmpTarget;
-				}
-				else if ((count($locations) == 0) && (strlen($s)== 0)) {
-					// do nothing
+		$myDirectory=opendir($dir);
+        while($name=readdir($myDirectory)) {
+			if (is_dir($dir.'/'.$name) && ($name != ".") && ($name != "..")) {
+				if ($root) {
+					$this->processDir($dir.'/'.$name, null, false);
 				}
 				else {
-					end($locations)->description .= $s;
-					end($locations)->description .= "\n";
+					$this->processDir($dir.'/'.$name, end($this->areas)->label, false);
 				}
 			}
-			foreach ($locations as $l) {
-				$l->description = trim($l->description);
-			}
-		}
-
-		function main($args) {
-			writeln("Everything is ok.");
-			return 0;
-		}
-
-		function cleanup() {
-			if (exists("./test")) rmdirRecurse("./test");
-			$areas = [];
-			$locations = [];
-		}
-
-		function startsWith($haystack, $needle) {
-			return !strncmp($haystack, $needle, strlen($needle));
-		}
-
-		function endsWith($haystack, $needle) {
-			$length = strlen($needle);
-			if ($length == 0) {
-				return true;
-			}
-			return (substr($haystack, -$length) === $needle);
 		}
 	}
+
+	function processMap($filename, $areaLabel, $areaName) {
+		foreach(split("\n", str_replace("\r\n", "\n", file_get_contents($filename))) as $s) {
+			if (startsWith($s, "# ")) {
+				assert(substr($s, 2) == $areaName);
+			}
+			else if (startsWith($s, "### ")) {
+				$this->locations[] = new Location();
+				$tmp = substr($s, 4);
+				end($this->locations)->name = split(" - ",$tmp)[0];
+				end($this->locations)->label = $areaLabel . "/" . split(" - ",$tmp)[1];
+			}
+			else if (startsWith($s, "* ")) {
+				$tmp = substr($s, 2);
+				$tmpAction = split(" - ",$tmp)[0];
+				$tmpTarget = split(" - ",$tmp)[1];
+				if (strpos($tmpTarget, '/') === false) $tmpTarget = $areaLabel . "/" . $tmpTarget;
+				end($this->locations)->actions[$tmpAction] = $tmpTarget;
+			}
+			else if ((count($this->locations) == 0) && (strlen($s)==0)) {
+				// do nothing
+			}
+			else {
+				end($this->locations)->description .= $s;
+				end($this->locations)->description .= "\n";
+			}
+		}
+		foreach ($this->locations as $l) {
+			$l->description = trim($l->description);
+		}
+	}
+}
+
+function startsWith($haystack, $needle) {
+	return !strncmp($haystack, $needle, strlen($needle));
+}
+
+function endsWith($haystack, $needle) {
+	$length = strlen($needle);
+	if ($length == 0) {
+		return true;
+	}
+	return (substr($haystack, -$length) === $needle);
+}
+
 ?>
--- locparse_test.php
+++ locparse_test.php
@@ -1,15 +1,17 @@
 <?php
 
-//require_once './PHPUnit/Framework.php';
 require_once './phpunit/vendor/autoload.php';
 require_once './locparse.php';
 
 class ParserTest extends PHPUnit_Framework_TestCase {
-	public function testPower() {
-		$my = new Parser();
 
-		// first test
-		$my->cleanup();
+	function cleanup() {
+		if (file_exists("./test")) rmdirr("./test");
+	}
+
+	public function testFirst() {
+		$my = new Parser();
+		$this->cleanup();
 		mkdir("./test");
 		$fp = fopen('./test/test.ht.md', 'w');
 		fwrite($fp, "
@@ -33,7 +35,6 @@ class ParserTest extends PHPUnit_Framework_TestCase {
 * Пойти на Голубую улицу - bluestreet
 
 ");
-
 		$my->processMap("./test/test.ht.md", "kront", "Кронт");
 		$this->assertEquals($my->locations[0]->name, "Голубая улица");
 		$this->assertEquals($my->locations[0]->label, "kront/bluestreet");
@@ -41,19 +42,17 @@ class ParserTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($my->locations[0]->actions["Пойти на Зелёную улицу"], "kront/greenstreet");
 		$this->assertEquals($my->locations[1]->name, "Зелёная улица");
 		$this->assertEquals($my->locations[1]->label, "kront/greenstreet");
-		$this->assertEquals($my->locations[1]->description, "Здесь посажены деревья.
-
-И грибы.
-
-И животноводство.");
+		$this->assertEquals($my->locations[1]->description, "Здесь посажены деревья.\n\nИ грибы.\n\nИ животноводство.");
 		$this->assertEquals($my->locations[1]->actions["Пойти на Голубую улицу"], "kront/bluestreet");
-		$my->cleanup();
-		writeln("passed");
+		$this->cleanup();
+	}
 
-		// second test
+	public function testSecond() {
+		$my = new Parser();
+		$this->cleanup();
 		mkdir("./test");
 		mkdir("./test/Кронт - kront");
-		$fp = fopen("./test/test.ht.md", 'w');
+		$fp = fopen("./test/Кронт - kront/map.ht.md", 'w');
 		fwrite($fp, "# Кронт");
 		mkdir("./test/Кронт - kront/Окрестности Кронта - outer");
 		$fp = fopen("./test/Кронт - kront/Окрестности Кронта - outer/map.ht.md", 'w');
@@ -80,25 +79,51 @@ class ParserTest extends PHPUnit_Framework_TestCase {
 
 ");
 		$my->processDir("./test", null, true);
+		$this->assertEquals(2, count($my->areas));
 		$this->assertEquals($my->areas[0]->name, "Кронт");
 		$this->assertEquals($my->areas[0]->label, "kront");
 		$this->assertEquals($my->areas[1]->name, "Окрестности Кронта");
 		$this->assertEquals($my->areas[1]->label, "kront-outer");
+		$this->assertEquals(2, count($my->locations));
 		$this->assertEquals($my->locations[0]->label, "kront-outer/bluestreet");
 		$this->assertEquals($my->locations[0]->description, "Здесь сидят гомосеки.");
 		$this->assertEquals($my->locations[0]->actions["Пойти на Зелёную улицу"], "kront-outer/greenstreet");
 		$this->assertEquals($my->locations[1]->name, "Зелёная улица");
 		$this->assertEquals($my->locations[1]->label, "kront-outer/greenstreet");
-		$this->assertEquals($my->locations[1]->description, "Здесь посажены деревья.
-
-И грибы.
-
-И животноводство.");
+		$this->assertEquals($my->locations[1]->description, "Здесь посажены деревья.\n\nИ грибы.\n\nИ животноводство.");
 		$this->assertEquals($my->locations[1]->actions["Пойти на Голубую улицу"], "kront/bluestreet");
 		$this->assertEquals($my->locations[1]->actions["Пойти на другую Голубую улицу"], "kront-outer/bluestreet");
-		$my->cleanup();
-		writeln("passed");
+		$this->cleanup();
 	}
+}
+
+function rmdirr($dirname)
+{
+	// Sanity check
+	if (!file_exists($dirname)) {
+		return false;
+	}
+
+	// Simple delete for a file
+	if (is_file($dirname) || is_link($dirname)) {
+		return unlink($dirname);
+	}
+
+	// Loop through the folder
+	$dir = dir($dirname);
+	while (false !== $entry = $dir->read()) {
+		// Skip pointers
+		if ($entry == '.' || $entry == '..') {
+			continue;
+		}
+
+		// Recurse
+		rmdirr($dirname . DIRECTORY_SEPARATOR . $entry);
+	}
+
+	// Clean up
+	$dir->close();
+	return rmdir($dirname);
 }
 
 ?>
