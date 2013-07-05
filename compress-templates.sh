8a9c32f7fd29a751d6c23606c8a076def07e6288
m1kc (Max Musatov) <m1kc@yandex.ru>
2013-07-06 02:40:49 +0400

    Ability to compress templates with twigged JavaScript.

--- compress-templates.sh
+++ compress-templates.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 for i in `ls templates_raw/`; do
-	echo "==>" htmlcompressor templates_raw/$i -o templates/$i --compress-css --compress-js
+	echo -n "${i}... "
 	htmlcompressor templates_raw/$i -o templates/$i --compress-css --compress-js
+	if [ $? == 0 ]
+	then
+		echo OK
+	else
+		htmlcompressor templates_raw/$i -o templates/$i --compress-css
+		if [ $? == 0 ]
+		then
+			echo OK, but failed to compress JS
+		else
+			echo FAIL
+			exit
+		fi
+	fi
 done
