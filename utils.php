dc4a8932abf584a211ed04282ac34d17881b3971
houjing <antidurov@mail.ru>
2013-07-27 23:26:37 +0300

    references #121

--- utils.php
+++ utils.php
@@ -666,8 +666,8 @@ function tf($s) {
 	$s = preg_replace('/^-(?= )|(?<= )-(?= )|---/', '&mdash;', $s);
 	$s = preg_replace('/--/', '&ndash;', $s);
 	$s = preg_replace('/-(?=\\d)/', '&minus;', $s);
-	$s = preg_replace('/"(?=\\w|[йЙёЁ])/', '&laquo;', $s);
-	$s = preg_replace('/(?<=\\w|[йЙёЁ])"/', '&raquo;', $s);
+	$s = preg_replace('/("|\&quot\;)(?=\\w|[йЙёЁ])/', '&laquo;', $s);
+	$s = preg_replace('/(?<=\\w|[йЙёЁ])("|\&quot\;)/', '&raquo;', $s);
 	return $s;
 }
 
