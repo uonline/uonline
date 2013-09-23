770a353f24b0c839e39045ef3588b1d9eef8f075
houjing <antidurov@mail.ru>
2013-09-24 01:14:36 +0300

    updated run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,4 +1,4 @@
 @echo off
 cls
-php vendor\phpunit\phpunit\composer\bin\phpunit --strict --verbose --coverage-html code-coverage-report tests
-node node_modules\nodeunit\bin\nodeunit tests_node\ --reporter verbose
+php vendor\phpunit\phpunit\composer\bin\phpunit --strict --verbose --coverage-html code-coverage-report tests_php
+npm test
