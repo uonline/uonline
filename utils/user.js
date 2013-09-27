uonline/uonline

commit b6c8c27f4449664fb1497fea84e07a0e92b08f94

user.js upd

utils/user.js
@@ -37,3 +37,78 @@ exports.userExists = function(dbConnection, username, callback, table)
 		}
 	);
 };
+
+exports.idExists = function(dbConnection, id, callback) {
+	dbConnection.query(
+		'SELECT count(*) FROM `uniusers` WHERE `id`= ?',
+		[id],
+		function (error, result) {
+			if (!!error) {
+				callback(error, undefined);
+			}
+			else {
+				callback(undefined, (result.rows[0].result > 0));
+			}
+		}
+	);
+};
+
+exports.userExists = function(dbConnection, user, callback) {
+	dbConnection.query(
+		'SELECT count(*) FROM `uniusers` WHERE `user` = ?',
+		[user],
+		function (error, result) {
+			if (!!error) {
+				callback(error, undefined);
+			}
+			else {
+				callback(undefined, (result.rows[0].result > 0));
+			}
+		}
+	);
+};
+
+exports.mailExists = function(dbConnection, mail, callback) {
+	dbConnection.query(
+		'SELECT count(*) FROM `uniusers` WHERE `mail` = ?',
+		[mail],
+		function (error, result) {
+			if (!!error) {
+				callback(error, undefined);
+			}
+			else {
+				callback(undefined, (result.rows[0].result > 0));
+			}
+		}
+	);
+};
+
+exports.sessionExists = function(dbConnection, sess, callback) {
+	dbConnection.query(
+		'SELECT count(*) FROM `uniusers` WHERE `sessid` = ?',
+		[sess],
+		function (error, result) {
+			if (!!error) {
+				callback(error, undefined);
+			}
+			else {
+				callback(undefined, (result.rows[0].result > 0));
+			}
+		}
+	);
+};
+
+exports.sessionActive = function(dbConnection, sess, callback) {
+	dbConnection.query(
+		'SELECT `sessexpire` > NOW() FROM `uniusers` WHERE `sessid` = ?',
+		[sess],
+		function (error, result) {
+			if (!!error) {
+				callback(error, undefined);
+			}
+			else {
+				callback(undefined, (result.rows[0].result > 0));
+			}
+		}
+	);
+};

commit 6b025e291fb4c4dd70eb5d4555fbbfc906f63969

Fix build broken by b6c8c27f4449664fb1497fea84e07a0e92b08f94.

utils/user.js
@@ -53,21 +53,6 @@ exports.idExists = function(dbConnection, id, callback) {
 	);
 };
 
-exports.userExists = function(dbConnection, user, callback) {
-	dbConnection.query(
-		'SELECT count(*) FROM `uniusers` WHERE `user` = ?',
-		[user],
-		function (error, result) {
-			if (!!error) {
-				callback(error, undefined);
-			}
-			else {
-				callback(undefined, (result.rows[0].result > 0));
-			}
-		}
-	);
-};
-
 exports.mailExists = function(dbConnection, mail, callback) {
 	dbConnection.query(
 		'SELECT count(*) FROM `uniusers` WHERE `mail` = ?',