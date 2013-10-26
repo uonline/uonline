5b92c1cc9cd2da749b5db2f0adf2fd463f015e55
m1kc (Max Musatov) <m1kc@yandex.ru>
2013-10-26 23:07:34 +0400

    Minor edits.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,42 +2,33 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    nodeunit: {
-      files: ['tests_node/*.js'],
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      root: {
-        src: ['*.js']
-      },
-      lib: {
-        src: ['utils/*.js']
-      },
-      test: {
-        src: ['tests_node/*.js']
-      },
-    },
-    jscoverage: {
-      options: {
-        inputDirectory: 'utils',
-        outputDirectory: 'utils-cov'
-      }
-    },
-  });
+	// Project configuration.
+	grunt.initConfig({
+		nodeunit: {
+			all: ['tests_node/*.js'],
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc',
+			},
+			all: {
+				src: ['Gruntfile.js', '*.js', 'utils/*.js', 'tests_node/*.js'],
+			},
+		},
+		jscoverage: {
+			options: {
+				inputDirectory: 'utils',
+				outputDirectory: 'utils-cov',
+			}
+		},
+	});
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jscoverage');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jscoverage');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'nodeunit']);
 
 };
