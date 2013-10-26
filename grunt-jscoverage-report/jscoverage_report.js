d0acf5b3577e88fe150bb69e7cacf507cbac1e64
m1kc (Max Musatov) <m1kc@yandex.ru>
2013-10-27 01:09:16 +0400

    Typo.

--- grunt-jscoverage-report/jscoverage_report.js
+++ grunt-jscoverage-report/jscoverage_report.js
@@ -11,7 +11,7 @@
 module.exports = function(grunt) {
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
-	grunt.registerTask('jscoverage_report', 'Shows jscoverage report.', function() {
+	grunt.registerTask('jscoverage_report', 'Show jscoverage report.', function() {
 		grunt.task.requires('nodeunit');
 		var jsc = require('jscoverage');
 		jsc.coverage(); // print summary info, cover percent
