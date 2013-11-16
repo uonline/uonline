bd213d1633b84509a57f5a3a6a6fe0447336c124
3bl3gamer <3bl3gamer@gmail.com>
2013-11-17 02:17:52 +0400

    The begining of js-migrations

---
+++ tests_node/migration.js
@@ -0,0 +1,124 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+"use strict";
+
+var config = require('../config.js');
+
+var jsc = require('jscoverage');
+jsc.enableCoverage(true);
+
+var mg = jsc.require(module, '../utils/migration.js');
+
+var async = require('async');
+
+var anyDB = require('any-db');
+var conn = null;
+
+var migrationData = [
+	[
+		['test_table', 'create', 'id INT'],
+	],
+	[
+		['test_table', 'addCol', 'col0 INT(1)'],
+		['test_table', 'addCol', 'col1 INT(2)'],
+	],
+	[
+		['test_table', 'addCol', 'col3 INT'],
+		['test_table', 'changeCol', 'col3', 'INT(3)'],
+		['test_table', 'renameCol', 'col3', 'col2'],
+	],
+	[
+		['test_table', 'addCol', 'col3 INT'],
+		['test_table', 'dropCol', 'col3'],
+	],
+];
+
+exports.setUp = function (done) {
+	conn = anyDB.createConnection(config.MYSQL_DATABASE_URL_TEST);
+	conn.query("DROP TABLE IF EXISTS test_table", done);
+	mg.setMigrationsData(migrationData);
+};
+
+exports.migrate = {
+	'testNoErrors': function(test) {
+		async.series([
+				function(callback) {mg.migrate(conn, 0, callback);},
+				function(callback) {conn.query("DESCRIBE test_table", callback);},
+				function(callback) {mg.migrate(conn, 1, callback);},
+				function(callback) {conn.query("DESCRIBE test_table", callback);},
+			],
+			function(error, result) {
+				test.ifError(error);
+			
+				test.ok(
+					result[1].rows.length === 1 &&
+					result[1].rows[0].Field === 'id', 'chould correctly perform first migration');
+			
+				test.ok(
+					result[3].rows.length === 3 &&
+					result[3].rows[0].Field === 'id' &&
+					result[3].rows[1].Field === 'col0' &&
+					result[3].rows[2].Field === 'col1', 'chould correctly add second migration');
+			
+				test.done();
+			}
+		);
+	},
+	'testErrors': function(test) {
+		async.series([
+				function(callback) {conn.query("DROP TABLE IF EXISTS test_table", callback);},
+				function(callback) {mg.migrate(conn, 1, callback);},
+			],
+			function(error, result) {
+				test.ok(error, 'Should return error if has failed to migrate');
+				test.done();
+			}
+		);
+	},
+};
+
+exports.migrateAll = function(test) {
+	async.series([
+			function(callback) {mg.migrateAll(conn, callback);},
+			function(callback) {conn.query("DESCRIBE test_table", callback);},
+		],
+		function(error, result) {
+			test.ifError(error);
+			var rows = result[1].rows;
+			test.ok(
+				rows.length === 4 &&
+				rows[0].Field === 'id' &&
+				rows[1].Field === 'col0' &&
+				rows[2].Field === 'col1' &&
+				rows[3].Field === 'col2' &&
+				rows[0].Type === 'int(11)' &&
+				rows[1].Type === 'int(1)' &&
+				rows[2].Type === 'int(2)' &&
+				rows[3].Type === 'int(3)', 'chould correctly perform migrations');
+			test.done();
+		}
+	);
+};
+
+exports.tearDown = function (done) {
+	conn.query("DROP TABLE IF EXISTS test_table", function() {
+		conn.end();
+		done();
+	});
+};
+
---
+++ utils/migration.js
@@ -0,0 +1,156 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+"use strict";
+
+var async = require('async');
+
+var tables = require('./tables.js');
+
+var FUNC_NAME_COLUMN = 1;
+var migrationData = [
+	[
+		/************** uniusers ****************/
+		['uniusers', 'create', 'id INT AUTO_INCREMENT, PRIMARY KEY (id)'],
+		['uniusers', 'addCol', 'location INT DEFAULT 1'],
+		['uniusers', 'addCol', 'permissions INT DEFAULT 0'],
+		['uniusers', 'addCol', 'user TINYTEXT'],
+		['uniusers', 'addCol', 'mail TINYTEXT'],
+		['uniusers', 'addCol', 'salt TINYTEXT'],
+		['uniusers', 'addCol', 'hash TINYTEXT'],
+		['uniusers', 'addCol', 'sessid TINYTEXT'],
+		['uniusers', 'addCol', 'sessexpire DATETIME'],
+		['uniusers', 'addCol', 'reg_time DATETIME'],
+
+		['uniusers', 'addCol', 'fight_mode INT DEFAULT 0'],
+		['uniusers', 'addCol', 'autoinvolved_fm INT DEFAULT 0'],
+		['uniusers', 'addCol', 'level INT DEFAULT 1'],
+		['uniusers', 'addCol', 'health INT DEFAULT 200'],
+		['uniusers', 'addCol', 'health_max INT DEFAULT 200'],
+		['uniusers', 'addCol', 'mana INT DEFAULT 100'],
+		['uniusers', 'addCol', 'mana_max INT DEFAULT 100'],
+		['uniusers', 'addCol', 'energy INT DEFAULT 50'],
+		['uniusers', 'addCol', 'power INT DEFAULT 3'],
+		['uniusers', 'addCol', 'defense INT DEFAULT 3'],
+		['uniusers', 'addCol', 'agility INT DEFAULT 3'], //ловкость
+		['uniusers', 'addCol', 'accuracy INT DEFAULT 3'], //точность
+		['uniusers', 'addCol', 'intelligence INT DEFAULT 5'], //интеллект
+		['uniusers', 'addCol', 'initiative INT DEFAULT 5'], //инициатива
+		['uniusers', 'addCol', 'exp INT DEFAULT 0'],
+		['uniusers', 'addCol', 'effects TEXT'],
+
+		/************** locations ****************/
+		['locations', 'create', 'id INT, PRIMARY KEY (id)'],
+		['locations', 'addCol', 'title TINYTEXT'],
+		['locations', 'addCol', 'goto TINYTEXT'],
+		['locations', 'addCol', 'description TEXT'],
+		['locations', 'addCol', 'area INT'],
+		['locations', 'addCol', 'picture TINYTEXT'],
+		['locations', 'addCol', 'default TINYINT(1) DEFAULT 0'],
+
+		/************** areas ****************/
+		['areas', 'create', 'id INT, PRIMARY KEY (id)'],
+		['areas', 'addCol', 'title TINYTEXT'],
+		['areas', 'addCol', 'description TEXT'],
+
+		/************** monster_prototypes ****************/
+		['monster_prototypes', 'create', 'id INT AUTO_INCREMENT, PRIMARY KEY (id)'],
+		['monster_prototypes', 'addCol', 'name TINYTEXT'],
+		['monster_prototypes', 'addCol', 'level INT'],
+		['monster_prototypes', 'addCol', 'power INT'],
+		['monster_prototypes', 'addCol', 'agility INT'],
+		['monster_prototypes', 'addCol', 'endurance INT'],
+		['monster_prototypes', 'addCol', 'intelligence INT'],
+		['monster_prototypes', 'addCol', 'wisdom INT'],
+		['monster_prototypes', 'addCol', 'volition INT'],
+		['monster_prototypes', 'addCol', 'health_max INT'],
+		['monster_prototypes', 'addCol', 'mana_max INT'],
+	],
+	[
+		/************** stats ****************/
+		['stats', 'create', 'time TIMESTAMP DEFAULT CURRENT_TIMESTAMP'],
+		['stats', 'addCol', 'gen_time DOUBLE'],
+		['stats', 'addCol', 'instance TINYTEXT'],
+		['stats', 'addCol', 'ip TINYTEXT'],
+		['stats', 'addCol', 'uagent TINYTEXT'],
+		['stats', 'addCol', 'url TEXT'],
+	],
+	[
+		/************** monsters ****************/
+		['monsters', 'rename', 'id', 'prototype'],
+		['monsters', 'rename', 'incarn_id', 'id'],
+	],
+];
+
+exports.getMigrationsData = function() {
+	return migrationData;
+};
+
+//for testing
+exports.setMigrationsData = function(data) {
+	migrationData = data;
+};
+
+exports.getNewestRevision = function() {
+	return exports.getMigrationsData().length-1;
+};
+
+exports.getCurrentRevision = function() {
+	return 0;
+};
+
+exports.setRevision = function() {
+	
+};
+
+exports.migrate = function(dbConnection, migration_id, callback) {
+	var migration = exports.getMigrationsData()[migration_id];
+	var i = 0;
+	function iteration(error) {
+		if (!!error)
+		{
+			callback(error, null);
+			return;
+		}
+		if (i == migration.length)
+		{
+			callback(null, true);
+			return;
+		}
+		var params = migration[i++].slice();
+		var funcName = params.splice(FUNC_NAME_COLUMN,1);
+		var func = tables[funcName];
+		params.unshift(dbConnection);
+		params.push(iteration);
+		func.apply(tables, params);
+	}
+	iteration(null);
+};
+
+exports.migrateAll = function(dbConnection, callback) {
+	var current = exports.getCurrentRevision();
+	var last = exports.getNewestRevision();
+	var i = current;
+	async.whilst(
+		function() {return i<=last;},
+		function(callback) {
+			exports.migrate(dbConnection, i++, callback);
+		},
+		callback
+	);
+};
+
